dcc4cbae7ce6f19ce1d310745e151509b5e56156
clopez <c88lopez@gmail.com>
2018-04-25 22:00:49 -0300

    quicksort

--- EjComplementarios.hs
+++ EjComplementarios.hs
@@ -5,3 +5,29 @@ myMaxComDiv (m, n) = if (m==n)
     else if (m>n) 
         then myMaxComDiv(m-n, n) 
         else myMaxComDiv(m, n-m)
+
+myMenoresOIg :: ([Int], Int) -> [Int]
+myMenoresOIg ([], _) = []
+myMenoresOIg (x:y, e) = 
+    if x <= e
+        then x:myMenoresOIg(y, e)
+        else myMenoresOIg(y, e)
+
+myMayores :: ([Int], Int) -> [Int]
+myMayores ([], _) = []
+myMayores (x:y, e) = 
+    if x > e
+        then x:myMayores(y, e)
+        else myMayores(y, e)
+
+myQuickSort :: [Int] -> [Int]
+myQuickSort [] = []
+myQuickSort (x:y) = 
+    myQuickSort(
+        myMenoresOIg(y, x)
+    )
+    ++
+    x:myQuickSort(
+        myMayores(y, x)
+    )
+    
